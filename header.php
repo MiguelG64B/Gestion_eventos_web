421190b58756e294870c868b8cc3874e9b01a84a
Edgar Miguel Bermudes Hernandez <79877254+MiguelG64B@users.noreply.github.com>
2022-06-09 13:17:05 -0500

    Delete header.php

--- header.php
+++
@@ -1,5 +0,0 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Gestion de eventos</a>
-        </div>
-    </nav>
