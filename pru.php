faff14fdba04a4c829fb5e8a953239e841f9f28b
Edgar Miguel Bermudes Hernandez <79877254+MiguelG64B@users.noreply.github.com>
2022-06-09 13:18:10 -0500

    Delete pru.php

--- pru.php
+++
@@ -1,8 +0,0 @@
-
-
-<form action="./php/asistencia.php" method="post">
-        <input type="text" id="correo" class="fadeIn second" name="correo" placeholder="email">
-        <input type="text" id="idevento" class="fadeIn third" name="idevento" placeholder="idevento">
-        <input type="text" id="idusuario" class="fadeIn third" name="idusuario" placeholder="idusuario">
-        <input type="submit" class="fadeIn fourth" value="probar">
-      </form>
